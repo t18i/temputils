efeeb3e974b7b1c5b1df3fe08e6ed4f32203909c
svkd <dsvk1996@gmail.com>
2021-05-09 12:31:45 +0530

    update:express multi core

---
+++ express/multiCoreNode.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const os = require("os");
+const cluster = require("cluster");
+
+const PORT = process.env.PORT || 80;
+
+const clusterWorkerSize = os.cpus().length;
+
+if (clusterWorkerSize > 1) {
+  if (cluster.isMaster) {
+    for (let i = 0; i < clusterWorkerSize; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", function (worker) {
+      console.log("Worker", worker.id, " has exitted.");
+    });
+  } else {
+    const app = express();
+    app.get("/", function (req, res) {
+      res.send("done");
+    });
+
+    app.listen(PORT, function () {
+      console.log(
+        `Express server listening on port ${PORT} and worker ${process.pid}`
+      );
+    });
+  }
+} else {
+  const app = express();
+  app.get("/", function (req, res) {
+    res.send("done");
+  });
+
+  app.listen(PORT, function () {
+    console.log(
+      `Express server listening on port ${PORT} with the single worker ${process.pid}`
+    );
+  });
+}
